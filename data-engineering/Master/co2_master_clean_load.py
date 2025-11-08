b8397838097dc61d0785fbe56530e8042b5eade7
asallh <ajaypalsallh@gmail.com>
2025-11-08 23:25:43 +0000

    initial load complete

---
+++ data-engineering/Master/co2_master_clean_load.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Clean Load
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT DISTINCT * FROM raw.master.co2_data;
+
+# COMMAND ----------
+
+
